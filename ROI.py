65e92c30185451130265a107143c349c0a802c07
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2019-03-22 19:10:29 +0530

    Update ROI.py
    Check the bottom line. We wrote out the bounding rectangle.

--- ROI.py
+++ ROI.py
@@ -81,9 +81,10 @@ no_of_objects=len(listy)*len(listx)/4
 #the rest is to shape these parts into images. and write trivial functions to trim further.
 #and then we run the predictor!
 
-
-
-im=im_th[listx[0]:listx[1],listy[0]:listy[1]]
+for i in range(no_of_objects):
+    rect = (listx[2*i-1], listy[2*i-1], listx[2*i],listy[2*i])
+    #now, we have a bounding rectangle around the object, 
+    #the image processing code goes here.
 
 
 
